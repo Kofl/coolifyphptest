70fd1db160c3c783bf6c1462c73bac3ecc20f59c
Kofl <thomas@kofler.tk>
2026-02-07 11:16:48 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,5 @@
 <?php
-$cat = "    /\\_/\\  " . PHP_EOL;
-$cat .= "   ( o.o ) " . PHP_EOL;
-$cat .= "    > ^ <" . PHP_EOL;
-$cat .= "   /|   |\\" . PHP_EOL;
-$cat .= "  (_|   |_)" . PHP_EOL;
-$cat .= "" . PHP_EOL;
 $cat .= "   Meow! 🐱" . PHP_EOL;
-
 echo $cat;
 ?>
 
